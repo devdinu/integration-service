0ee9742612de2481f9240cc7962478c8d4ed5759
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-08-20 12:07:56 +0530

    Enabling cors, and sending content of the response when we create payment

--- payment.py
+++ payment.py
@@ -2,6 +2,7 @@ from instamojo_wrapper import Instamojo
 import os
 import pdb
 import requests
+import json
 
 class PaymentClient:
 
@@ -10,12 +11,12 @@ class PaymentClient:
         self.auth = os.environ.get('AUTH')
         self.salt = os.environ.get('SALT')
 
-    def create_request(self, _farmer_id, _amount, _purpose, _email, _redirect_url):
+    def create_request(self, _farmer_id, _amount, _purpose):
         headers = { "X-Api-Key": self.api, "X-Auth-Token": self.auth }
-        payload = { 'purpose': _purpose, 'amount': _amount }
+        payload = { 'purpose': _purpose, 'amount': _amount, 'buyer_email': "abc@gmail.com", 'redirect_url': "http://localhost:3000/"}
         pay_request_response = requests.post("https://test.instamojo.com/api/1.1/payment-requests/", data=payload, headers=headers)
         print(pay_request_response)
-        return pay_request_response['longurl']
+        return pay_request_response.json()
 
     def get_status(self, payment_req_id):
         print(response)
--- service.py
+++ service.py
@@ -1,11 +1,23 @@
 import os
-from bottle import request, get, post, run
+from bottle import request, get, post, run, response
 from twilio_client import *
 from twilio import twiml
 from payment import PaymentClient
 
 client = TwilioClient()
 paymentClient = PaymentClient()
+
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            return fn(*args, **kwargs)
+
+    return _enable_cors
+
 
 @get('/ping')
 def ping():
@@ -23,8 +35,9 @@ def get_response_for_farmer():
     response = twiml.Response().say("Welcome to Fund Farmers", voice='alice')
     return response
 
+@enable_cors
 @post('/payments/<farmer_id>/create')
 def create_payment_request(farmer_id):
-    return paymentClient.create_request(farmer_id, request.forms.get('amount'), request.forms.get('purpose'), request.forms.get('buyer_email'), request.forms.get('redirect_url'))
+    return paymentClient.create_request(farmer_id, request.forms.get('amount'), request.forms.get('purpose'))
 
 run(host='localhost', reloader=True, port=8080)
